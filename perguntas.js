c333cd98c6fcc0b1a4c63dc4cc0ffcbef3c3a871
bruruno02 <00001102072680sp@al.educacao.sp.gov.br>
2024-11-21 11:54:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Risadolas',
+    'por que a galinha atravessou a rua?',
+    'porque jacaré não voa kkkkkk'
+)
+
+criaCartao(
+    'Geografia',
+    'Como o Silvio Santos é chamado nos Emirados árabes?',
+    'سيلفيو سانتوس'
+)
+
+criaCartao(
+    'piadoca kkkk',
+    'sabe por quê não existe flor preta?',
+    'Segundo Enio Luiz, pesquisador da UFSC (Universidade Federal de Santa Catarina), ao Globo Rural, a cor preta da flor impediria a reprodução, a partir das sementes, por não atrair polinizadores – que são pequenos animais, como as abelhas, que fazem o trabalho de transferência do pólen.'
+)
+
+criaCartao(
+    'muito engraçado',
+    'sabe como deixar um carteiro triste?',
+    'Você mata a família dele'
+)
